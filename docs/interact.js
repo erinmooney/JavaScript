92435385021da71330471b5ca69a6262b8195180
unknown <erinmooney3746@gmail.com>
2022-11-14 22:11:38 -0500

    boxes not working yet

--- docs/interact.js
+++ docs/interact.js
@@ -7,9 +7,15 @@ window.addEventListener('DOMContentLoaded',init,false);
 function init() {
     alert('Welcome! A character I like just died. So, here is a fun button to help distract from my woes.');
     var buttons = document.getElementsByTagName("button")
-    buttons[0].addEventListener('click', changeComment, false)
+    buttons[0].addEventListener('click', myFunction, false)
+    
+    var fieldset = document.getElementsByTagName("input");
+    for (var i = 0; i < fieldset.length; i++) {
+        fieldset[i].addEventListener('click', toggle, false);
+        }
 }
 
+/* Button 0 */
 function myFunction() {
     var x = document.getElementById("commentToggle");    
   if (x.innerHTML === "Thanks for visiting") 
@@ -20,3 +26,44 @@ function myFunction() {
   }
 }
     
+/* Checkboxes */
+function toggle() {
+    var id = this.id;
+    switch (id) {
+        case "niceToggle": {
+            var chars = document.getElementsByClassName("nice");
+            for (var i = 0; i < nice.length; i++) {
+                nice[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "sadToggle": {
+            var places = document.getElementsByClassName("sadFace");
+            for (var i = 0; i < sad.length; i++) {
+                sad[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "needToggle": {
+            var objects = document.getElementsByClassName("joy");
+            for (var i = 0; i < need.length; i++) {
+                need[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "questionsToggle": {
+            var objects = document.getElementsByClassName("goodQ");
+            for (var i = 0; i < question.length; i++) {
+                question[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "ouchToggle": {
+            var objects = document.getElementsByClassName("fineThen");
+            for (var i = 0; i < fine.length; i++) {
+                fine[i].classList.toggle("on")
+            }
+        };
+        break;
+    }
+  }
